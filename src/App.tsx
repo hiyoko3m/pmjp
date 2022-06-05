d76df41b16ed667f424a76e9db6671d059c25448
hiyoko3m <hiyoko3m.tech@gmail.com>
2022-06-06 00:53:56 +0900

    refactor: extract the date picker into a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,16 @@
 import React from "react";
 import { Title, Explanation } from "./components/common";
 import Stack from "@mui/material/Stack";
-import Autocomplete from "@mui/material/Autocomplete";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
+import {AdJpDate, AdJpPartialDate, AdJpDateFormProps, AdJpDateForm} from "./components/date_form";
 
 function App() {
-  const [adYear, setAdYear] = React.useState<string>("2001");
-  const [adMonth, setAdMonth] = React.useState<string>("1");
-  const [adDate, setAdDate] = React.useState<string>("31");
-
-  const inputAdYear = React.useCallback(
-    (_event: React.SyntheticEvent, value: string, _reason: string) => {
-      setAdYear(value);
-      console.log("inputAdYear");
-    },
-    [setAdYear]
-  );
-
-  const inputAdMonth = React.useCallback(
-    (_event: React.SyntheticEvent, value: string, _reason: string) => {
-      setAdMonth(value);
-      setAdDate(value);
-      console.log("inputAdMonth");
-    },
-    [setAdMonth]
-  );
-
-  const inputAdDate = React.useCallback(
-    (_event: React.SyntheticEvent, value: string, _reason: string) => {
-      setAdDate(value);
-      console.log(`inputAdDate ${value} ${_reason}`);
-    },
-    [setAdDate]
-  );
+  const [adDate, setAdDate] = React.useState<AdJpPartialDate>({adYear: undefined, adMonth: undefined, adDay: undefined});
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setAdMonth(adYear);
     console.log("handleSubmit");
   }
 
@@ -59,58 +25,12 @@ function App() {
       </Explanation>
       <Container maxWidth={false}>
         <Stack component="form" onSubmit={handleSubmit} alignItems="center" mt={2} spacing={2}>
-          <Stack direction="row" spacing={2}>
-            <Autocomplete
-              disablePortal
-              freeSolo
-              id="ad-year"
-              options={options}
-              renderInput={(params) => <TextField {...params} label="年" />}
-              sx={{width: "110px"}}
-              onInputChange={inputAdYear}
-              inputValue={adYear}
-            />
-            <Autocomplete
-              disablePortal
-              freeSolo
-              id="ad-month"
-              options={options}
-              renderInput={(params) => <TextField {...params} label="月" />}
-              sx={{width: "80px"}}
-              onInputChange={inputAdMonth}
-              inputValue={adMonth}
-            />
-            <Autocomplete
-              disablePortal
-              freeSolo
-              id="ad-date"
-              options={options}
-              renderInput={(params) => <TextField {...params} label="日" />}
-              sx={{width: "80px"}}
-              onInputChange={inputAdDate}
-              inputValue={adDate}
-            />
-          </Stack>
-          <FormControl>
-            <RadioGroup
-              row
-              name="year-type-radio-buttons-group"
-            >
-              <FormControlLabel value="ad" control={<Radio size="small" />} label="西暦" />
-              <FormControlLabel value="jp" control={<Radio size="small" />} label="和暦" />
-            </RadioGroup>
-          </FormControl>
+          <AdJpDateForm adJpDateValue={adDate} onChange={(newDate) => setAdDate(newDate)} />
           <Button variant="contained" type="submit">総理大臣を表示</Button>
         </Stack>
       </Container>
     </>
   );
 }
-
-const options = [
-  { label: "1", adYear: "2001" },
-  { label: "2", adYear: "2002" },
-  { label: "3", adYear: "2003" },
-];
 
 export default App;
--- src/components/date_form.tsx
+++ src/components/date_form.tsx
@@ -1,13 +1,114 @@
 import React from "react";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 
+
+interface AdJpDate {
+  adYear: number,
+  adMonth: number,
+  adDay: number,
+}
+
+interface AdJpPartialDate {
+  adYear?: number,
+  adMonth?: number,
+  adDay?: number,
+}
+
+interface AdJpDateFormProps {
+  adJpDateValue: AdJpPartialDate,
+  onChange: (arg: AdJpPartialDate) => void,
+  choiceBegin?: AdJpDate,
+  choiceEnd?: AdJpDate,
+}
 
 /**
  * can choose which to show A.D. or Japanese calendar via selector
  */
-export function ChoicableDateForm() {
+export function AdJpDateForm(props: AdJpDateFormProps) {
+  const {adYearShown, adMonthShown, adDayShown} = (() => {
+    const convert = (num?: number) => {
+      if (num) {
+        return num.toString();
+      } else {
+        return undefined;
+      }
+    };
+    return {
+      adYearShown: convert(props.adJpDateValue.adYear),
+      adMonthShown: convert(props.adJpDateValue.adMonth),
+      adDayShown: convert(props.adJpDateValue.adDay),
+    };
+  })();
+
+  const handleChangeYear = React.useCallback(
+    (_event: React.SyntheticEvent, value: string, _reason: string) => {
+      props.onChange({...props.adJpDateValue, adYear: parseInt(value)});
+    }, [props.adJpDateValue, props.onChange]
+      );
+
+  const handleChangeMonth = React.useCallback(
+    (_event: React.SyntheticEvent, value: string, _reason: string) => {
+      props.onChange({...props.adJpDateValue, adMonth: parseInt(value)});
+    }, [props.adJpDateValue, props.onChange]
+      );
+
+  const handleChangeDay = React.useCallback(
+    (_event: React.SyntheticEvent, value: string, _reason: string) => {
+      props.onChange({...props.adJpDateValue, adDay: parseInt(value)});
+    }, [props.adJpDateValue, props.onChange]
+      );
+
   return (
     <>
-      
+          <Stack direction="row" spacing={2}>
+            <Autocomplete
+              disablePortal
+              freeSolo
+              id="ad-year"
+              options={["2000"]}
+              renderInput={(params) => <TextField {...params} label="年" />}
+              sx={{width: "110px"}}
+              onInputChange={handleChangeYear}
+              inputValue={adYearShown}
+            />
+            <Autocomplete
+              disablePortal
+              freeSolo
+              id="ad-month"
+              options={["1"]}
+              renderInput={(params) => <TextField {...params} label="月" />}
+              sx={{width: "80px"}}
+              onInputChange={handleChangeMonth}
+              inputValue={adMonthShown}
+            />
+            <Autocomplete
+              disablePortal
+              freeSolo
+              id="ad-day"
+              options={["1"]}
+              renderInput={(params) => <TextField {...params} label="日" />}
+              sx={{width: "80px"}}
+              onInputChange={handleChangeDay}
+              inputValue={adDayShown}
+            />
+          </Stack>
+          <FormControl>
+            <RadioGroup
+              row
+              name="year-type-radio-buttons-group"
+            >
+              <FormControlLabel value="ad" control={<Radio size="small" />} label="西暦" />
+              <FormControlLabel value="jp" control={<Radio size="small" />} label="和暦" />
+            </RadioGroup>
+          </FormControl>
     </>
   );
 }
+
+export type { AdJpDate, AdJpPartialDate, AdJpDateFormProps };
